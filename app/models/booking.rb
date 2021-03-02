6b650e1bc5f8b287158754bf3d54c945611786a4
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-02 13:24:19 -0500

    added validators for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,8 @@ class Booking < ApplicationRecord
   has_one :review
   has_one :payment
   has_one :report
+  validates :user, presence: true
+  validates :lesson, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
