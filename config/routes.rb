9e130253d468a75794a12387b58ff9f7301f2d45
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-02 13:53:23 -0500

    added routing for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :lessons, only: [ :index, :new, :create, :edit, :update, :destroy ]
+  resources :lessons, only: [ :index, :new, :create, :edit, :update, :destroy ] do
+    resources :bookings, only: [ :new, :create, ]
+  end
+  
+  resources :bookings, only: [ :show, :edit, :update ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
