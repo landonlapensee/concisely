f9aac487afce69082db304243b4afc9d390967a9
Nicolas Tsu <nicolas.tsu@hec.ca>
2021-03-02 14:15:34 -0500

    added the new and create actions in the lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,9 +3,19 @@ class LessonsController < ApplicationController
   end
 
   def new
+    @lesson = Lesson.new
   end
 
   def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.user = current_user
+    @lesson.save
+
+    if @lesson.save
+      redirect_to @lesson, notice: "Your lesson has been created"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +25,11 @@ class LessonsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def lessons_params
+    params.require(:lesson).permit(:user_id, :description, :industry, :price) # is :user_id relevant ?
   end
 end
