f19f38aeabcba188a1ea1bd8402112b9be6b1bca
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-02 14:30:03 -0500

    added new, create actions. Plus added views.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,5 +4,26 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
+  def new
+    @lesson = Lesson.find(params[:lesson_id])
+    @booking = Booking.new
+  end
   
+  def create
+    @booking = Booking.new(booking_params)
+    @lesson = Lesson.find(params[:lesson_id])
+    @booking.lesson = @lesson
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time)
+  end
 end
