ece8819f2653ca915ed75f264f4e0e7ac2ff434f
Landon Lapensee <landonlapensee@gmail.com>
2021-03-02 15:22:41 -0800

    Added content to report actions, and added a simple form to the new view

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :booking
+
+  validates :description, :rating, :booking_id, :date, presence: true
+  validates :rating, :inclusion => { :in => (1..10) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    def can_create_a_report_for?(booking)
+      self == booking.lesson.user_id
+    end
 end
