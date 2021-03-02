50d37007170eba48a48b78fc758df446cb6d24c2
Landon Lapensee <landonlapensee@gmail.com>
2021-03-02 07:45:20 -0800

    Added lessons controller and routing

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,19 @@
+class LessonsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'lessons/index'
+  get 'lessons/new'
+  get 'lessons/create'
+  get 'lessons/edit'
+  get 'lessons/update'
+  get 'lessons/destroy'
   devise_for :users
   root to: 'pages#home'
+
+  resources :lessons, only: [ :index, :new, :create, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
