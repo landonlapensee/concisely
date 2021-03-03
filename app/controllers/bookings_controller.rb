d0b4b7123cf9139081301a857413481c9936e383
georgiana chiriac <georgianachiriacweb@gmail.com>
2021-03-03 17:54:21 -0500

    create booking functionality in the index modal

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-
   def show
     @booking = Booking.find(params[:id])
   end
@@ -8,22 +7,23 @@ class BookingsController < ApplicationController
     @lesson = Lesson.find(params[:lesson_id])
     @booking = Booking.new
   end
-  
+
   def create
     @booking = Booking.new(booking_params)
     @lesson = Lesson.find(params[:lesson_id])
+    @booking.duration = 60
     @booking.lesson = @lesson
     @booking.user = current_user
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to my_bookings_path
     else
       render :new
     end
   end
-  
+
   private
 
   def booking_params
-    params.require(:booking).permit(:start_time, :end_time)
+    params.require(:booking).permit(:start_time)
   end
 end
