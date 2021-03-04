8b27f4aec3097373e25762b8c6ee7ba71a682348
Nicolas Tsu <nicolas.tsu@hec.ca>
2021-03-04 17:58:20 -0500

    modified the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,17 @@ User.destroy_all
 # user = users.first
 # user["email"]
 
-#Coaches
 coaches = []
-emails = ["a@a.a", "b@b.b", "c@c.c", "d@d.d", "e@e.e", "f@f.f", "g@g.g", "h@h.h", "i@i.i"]
-emails.each do |email|
+
+users.each do |userhash|
   coaches << User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: email,
+    first_name: userhash["name"]["first"],
+    last_name: userhash["name"]["last"],
+    email: userhash["email"],
     password: "123456",
-    image: "https://randomuser.me/api/portraits/men/34.jpg"
+    image: userhash["picture"]["large"]
   )
-end 
+end
 
 coaches.each do |user|
   Lesson.create!( 
@@ -35,8 +34,24 @@ coaches.each do |user|
   )
 end
 
+
+#Coaches
+# coaches = []
+# emails = ["a@a.a", "b@b.b", "c@c.c", "d@d.d", "e@e.e", "f@f.f", "g@g.g", "h@h.h", "i@i.i"]
+# emails.each do |email|
+#   coaches << User.create!(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: email,
+#     password: "123456",
+#     image: "https://randomuser.me/api/portraits/men/34.jpg"
+#   )
+# end 
+
+
 #student
 student = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "s@s.s", password: "123456", image: "https://randomuser.me/api/portraits/men/34.jpg")
+coach = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "c@c.c", password: "123456", image: "https://randomuser.me/api/portraits/men/34.jpg")
 
 #future bookings
 booking1 = Booking.create!(
@@ -60,6 +75,7 @@ booking3 = Booking.create!(
   lesson: Lesson.all.sample
 )
 
+#past bookings
 booking4 = Booking.create!(
   user: student,
   start_time: DateTime.strptime("09/01/2021 17:00", "%d/%m/%Y %H:%M"),
@@ -83,21 +99,21 @@ booking6 = Booking.create!(
 
 #reports
 report1 = Report.create!(
-  date: DateTime.strptime("09/03/2021 20:00", "%d/%m/%Y %H:%M"),
+  date: DateTime.strptime("09/01/2021 20:00", "%d/%m/%Y %H:%M"),
   booking: booking1,
   description: "Very good job !",
   rating: 5
 )
 
 report2 = Report.create!(
-  date: DateTime.strptime("16/03/2021 20:00", "%d/%m/%Y %H:%M"),
+  date: DateTime.strptime("16/01/2021 20:00", "%d/%m/%Y %H:%M"),
   booking: booking2,
   description: "Very good job !",
   rating: 5
 )
 
 report3 = Report.create!(
-  date: DateTime.strptime("23/03/2021 20:00", "%d/%m/%Y %H:%M"),
+  date: DateTime.strptime("23/01/2021 20:00", "%d/%m/%Y %H:%M"),
   booking: booking3,
   description: "Very good job !",
   rating: 5
