9650daef0d90c507efccfbe6818a902ddf617bb9
georgiana chiriac <georgianachiriacweb@gmail.com>
2021-03-04 15:11:13 -0500

    removed remanants of number_of_lessons

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,6 +26,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_time, :number_of_lessons)
+    params.require(:booking).permit(:start_time)
   end
 end
