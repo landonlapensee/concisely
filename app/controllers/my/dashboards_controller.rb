09937e54b140ee3d35c04582b7e9b6138a3652e9
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-04 13:53:39 -0500

    changed some lesson model associations to allow for .bookings to be called on lesson instance. Added User model methods for readability

--- app/controllers/my/dashboards_controller.rb
+++ app/controllers/my/dashboards_controller.rb
@@ -1,4 +1,15 @@
 class My::DashboardsController < ApplicationController
   def show
-  end 
+    if current_user.is_a_coach?
+      my_bookings = current_user.bookings.all
+      @my_listings = current_user.lessons.all
+      @my_listing_bookings = @my_listings.map do |listing|
+        listing.bookings.all
+      end
+      @my_bookings = my_bookings + @my_listings
+      raise
+    else
+      @my_bookings = current_user.bookings.all
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
     def can_create_a_report_for?(booking)
       self == booking.lesson.user_id
     end
+
+    def has_made_a_booking?
+      self.bookings.first
+    end
+
+    def is_a_coach?
+      self.lessons.first
+    end
 end
