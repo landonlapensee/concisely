0373a3fa94e4388bb61375cbe2d7625144567091
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-05 14:01:45 -0500

    finished upcoming-booking logic

--- app/controllers/my/bookings_controller.rb
+++ app/controllers/my/bookings_controller.rb
@@ -1,16 +1,14 @@
 class My::BookingsController < ApplicationController
   def index
     if current_user.is_a_coach?
-      my_bookings = current_user.bookings.all
-      @my_listings = current_user.lessons.all
-      @my_listings.each do |listing|
-        @my_listing_bookings = listing.bookings.all.each do |booking|
-          booking
-        end
-      end
-      @my_bookings = my_bookings + @my_listing_bookings
+      my_bookings = current_user.bookings
+      my_listings = current_user.lessons
+      my_listing_bookings = my_listings.map { |listing| listing.bookings }.flatten
+      @my_bookings = my_bookings + my_listing_bookings
     else
-      @my_bookings = current_user.bookings.all
+      @my_bookings = current_user.bookings
     end
+    sorted_bookings = @my_bookings.sort { |x| x.end_time }
+    @upcoming_booking = sorted_bookings.find { |b| b.end_time >= Time.now }
   end
 end
