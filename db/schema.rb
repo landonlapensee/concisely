bcd3388d89609bd7248380d29d3a1fe583d25d2f
Landon Lapensee <landonlapensee@gmail.com>
2021-03-04 20:29:41 -0800

    added to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_03_04_201258) do
-=======
 ActiveRecord::Schema.define(version: 2021_03_04_220952) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,10 +98,10 @@ ActiveRecord::Schema.define(version: 2021_03_04_220952) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.text "bio"
     t.string "country_code"
     t.string "city"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
