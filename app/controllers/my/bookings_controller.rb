2ca7e05c53eb0aed0726396ec5821323cddc6d14
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-05 11:56:31 -0500

    changed dashboard link in navbar to my_bookings_path, removed dashboard show and transfered logic to my_bookings#index

--- app/controllers/my/bookings_controller.rb
+++ app/controllers/my/bookings_controller.rb
@@ -1,5 +1,16 @@
 class My::BookingsController < ApplicationController
   def index
-    @bookings = Booking.where(user_id: current_user)
+    if current_user.is_a_coach?
+      my_bookings = current_user.bookings.all
+      @my_listings = current_user.lessons.all
+      @my_listings.each do |listing|
+        @my_listing_bookings = listing.bookings.all.each do |booking|
+          booking
+        end
+      end
+      @my_bookings = my_bookings + @my_listing_bookings
+    else
+      @my_bookings = current_user.bookings.all
+    end
   end
 end
