8709102fc5d6ae73063148201e5ff8c992b97a49
Nicolas Tsu <nicolas.tsu@hec.ca>
2021-03-08 12:06:15 -0500

    create message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_220952) do
+ActiveRecord::Schema.define(version: 2021_03_08_170031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,16 @@ ActiveRecord::Schema.define(version: 2021_03_04_220952) do
     t.index ["user_id"], name: "index_lessons_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.bigint "recipient_id", null: false
+    t.bigint "sender_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipient_id"], name: "index_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
+
   create_table "reports", force: :cascade do |t|
     t.date "date"
     t.text "description"
@@ -110,6 +120,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_220952) do
   add_foreign_key "bookings", "lessons"
   add_foreign_key "bookings", "users"
   add_foreign_key "lessons", "users"
+  add_foreign_key "messages", "users", column: "recipient_id"
+  add_foreign_key "messages", "users", column: "sender_id"
   add_foreign_key "reports", "bookings"
   add_foreign_key "reviews", "bookings"
 end
