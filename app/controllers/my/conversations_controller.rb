0c09a6d34975addd8414505da0c66f583f389f0d
Nicolas Tsu <nicolas.tsu@hec.ca>
2021-03-08 15:01:18 -0500

    added sender name in front of messages to understand who sent the message

--- app/controllers/my/conversations_controller.rb
+++ app/controllers/my/conversations_controller.rb
@@ -1,9 +1,31 @@
 class My::ConversationsController < ApplicationController
   #my/conversations?user_id=42
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find_by(id: params[:user_id]) || first_conversation_user
     @messages = @user.messages_with(current_user)
+    @message = Message.new
   end
   
+  def create
+    @user = User.find(params[:user_id])
+    @message = Message.new(message_params)
+    @message.user = @user
+    @message.user = current_user
+    if @message.save 
+      redirect_to my_conversations_path(current_user)
+    else
+      render "my/conversations"
+    end
+  end
+
+  private 
+
+  def first_conversation_user
+    current_user.conversation_users.first
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
 
