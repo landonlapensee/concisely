5786ffbdac838f199e4590dc694b589b3d4f824c
georgiana chiriac <georgianachiriacweb@gmail.com>
2021-03-10 11:51:02 -0500

    create upcoming and past scopes in booking model, and the functionality for displaying these in my/covnversations, under booking section

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,7 +31,6 @@ import { dailyVideoChatTriggering } from '../plugins/daily.js';
 
 // Date Picker
 import { initFlatpickr } from "../plugins/flatpickr";
-initFlatpickr();
 
 document.addEventListener('turbolinks:load', () => {
   const scrollBar = document.getElementById('messages-scrollbar')
@@ -39,6 +38,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   dailyVideoChatTriggering();
+  initFlatpickr();
 });
 
 require("trix")
