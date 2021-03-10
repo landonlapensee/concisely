89ff32617672741346bfea8c825f8b7fcce75522
Laurier StAubin <st-aubinlaurier@hotmail.com>
2021-03-10 10:16:26 -0500

    logic for reports is completed, addign styling now

--- app/controllers/my/reports_controller.rb
+++ app/controllers/my/reports_controller.rb
@@ -1,5 +1,6 @@
 class My::ReportsController < ApplicationController
   def index
     @reports = current_user.all_reports
+    @report = Report.find_by(id: params[:report_id]) || @reports.first
   end
 end
